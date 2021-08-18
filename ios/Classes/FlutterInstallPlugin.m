30cdc235e116bfe55ce94274189c458352b82ec7
chenfuhui <chenfuhui@xxbmm.com>
2021-08-18 13:13:38 +0800

    channelName

--- ios/Classes/FlutterInstallPlugin.m
+++ ios/Classes/FlutterInstallPlugin.m
@@ -3,7 +3,7 @@
 @implementation FlutterInstallPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel
-                                     methodChannelWithName:@"flutter_install"
+                                     methodChannelWithName:@"install_plugin"
                                      binaryMessenger:[registrar messenger]];
     FlutterInstallPlugin* instance = [[FlutterInstallPlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
@@ -26,24 +26,24 @@
         result([FlutterError errorWithCode:@"-2" message:@"url参数错误: 类型错误" details:@""]);
         return;
     }
-    
+
     NSString *urlString = (NSString *)args;
     if (urlString.length == 0) {
         result([FlutterError errorWithCode:@"-1" message:@"url参数错误: 不能为空" details:@""]);
         return;
     }
-    
+
     NSURL *url = [NSURL URLWithString:urlString];
     if (!url) {
         result([FlutterError errorWithCode:@"-3" message:@"获取URL失败" details:[NSString stringWithFormat:@"URL String: %@", urlString]]);
         return;
     }
-    
+
     if (![[UIApplication sharedApplication] canOpenURL:url]) {
         result([FlutterError errorWithCode:@"-5" message:@"Url 无法被开启" details:[NSString stringWithFormat:@"URL String: %@", url.absoluteString]]);
         return;
     }
-    
+
     if (@available(iOS 10.0, *)) {
         [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:^(BOOL success) {
             if (success) {
