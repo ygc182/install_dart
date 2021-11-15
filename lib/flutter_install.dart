1f757fb2c55fb70cc246449d9c5c6ef795a5c27a
cfh <chenfuhui@xxbmm.com>
2021-11-15 18:45:12 +0800

    migrate nullsafety

--- lib/flutter_install.dart
+++ lib/flutter_install.dart
@@ -6,13 +6,13 @@ class FlutterInstall {
   static const MethodChannel _channel = const MethodChannel('install_plugin');
 
   /// for Android: update apk
-  static Future<String> updateApk(url, title) async {
+  static Future<String?> updateApk(String url, String? title) async {
     Map<String, dynamic> params = {'url': url, 'title': title};
     return await _channel.invokeMethod('downloadApk', params);
   }
 
   /// for iOS: check update
-  static Future<String> update(String url) async {
+  static Future<String?> update(String url) async {
     return await _channel.invokeMethod("update", url);
   }
 }
