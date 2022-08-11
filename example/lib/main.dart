ba6a7f569cd21ec1f02ca932a033711ef9aea2d2
cfh <chenfuhui@xxbmm.com>
2022-08-11 17:02:19 +0800

    dart fix

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_install/flutter_install.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('FlutterInstallPlugin example app'),
         ),
         body: Center(
-          child: FlatButton(onPressed: update, child: Text("install")),
+          child: FlatButton(onPressed: update, child: Text('install')),
         ),
       ),
     );
@@ -34,10 +34,10 @@ class _MyAppState extends State<MyApp> {
   void checkToInstall() async {
     if (await Permission.storage.request().isGranted) {
       FlutterInstall.updateApk(
-          "https://f.meishi.51baibao.com/mt/e07f374c-0aa9-11ea-b032-0242ac1e0002/xxbmm--v1.8.0-4.apk",
+          'https://f.meishi.51baibao.com/mt/e07f374c-0aa9-11ea-b032-0242ac1e0002/xxbmm--v1.8.0-4.apk',
           '测试名称'
       ).then((result) {
-        print("result: $result");
+        print('result: $result');
       });
     }
   }
@@ -46,7 +46,7 @@ class _MyAppState extends State<MyApp> {
     if (Platform.isAndroid) {
       checkToInstall();
     } else if (Platform.isIOS) {
-      FlutterInstall.update("https://itunes.apple.com/cn/app/%E5%86%8D%E6%83%A0%E5%90%88%E4%BC%99%E4%BA%BA/id1375433239?l=zh&ls=1&mt=8");
+      FlutterInstall.update('https://itunes.apple.com/cn/app/%E5%86%8D%E6%83%A0%E5%90%88%E4%BC%99%E4%BA%BA/id1375433239?l=zh&ls=1&mt=8');
     }
   }
 }
--- test/flutter_install_test.dart
+++ test/flutter_install_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_install/flutter_install.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_install');
